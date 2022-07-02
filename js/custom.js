67b08bfc28b403fcccac5d68249d22150471031a
Ian Collada <Ian9Franco>
2022-07-02 03:32:05 -0300

    version beta2.0

--- js/custom.js
+++ js/custom.js
@@ -102,9 +102,6 @@ jQuery(document).ready(function( $ ) {
       
   });
   
-  $('.back-to-top').click(function(){
-      $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-      return false;
-  });
+ 
 
 });
